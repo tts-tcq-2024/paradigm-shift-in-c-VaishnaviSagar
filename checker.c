f006d91c9b1b7e6869b9b98ce4754f9997f792ec
VaishnaviSagar <67277305+VaishnaviSagar@users.noreply.github.com>
2024-06-28 19:53:40 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -7,10 +7,38 @@
 #define SOC_MAX 80
 #define CHARGE_RATE_MAX 0.8
 
+int isInRange(float value, float min, float max) {
+    return (value >= min && value <= max);
+}
+
+int checkTemperature(float temperature) {
+    if (!isInRange(temperature, TEMP_MIN, TEMP_MAX)) {
+        printf("Temperature out of range!\n");
+        return 0;
+    }
+    return 1;
+}
+
+int checkStateOfCharge(float soc) {
+    if (!isInRange(soc, SOC_MIN, SOC_MAX)) {
+        printf("State of Charge out of range!\n");
+        return 0;
+    }
+    return 1;
+}
+
+int checkChargeRate(float chargeRate) {
+    if (chargeRate > CHARGE_RATE_MAX) {
+        printf("Charge Rate out of range!\n");
+        return 0;
+    }
+    return 1;
+}
+
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-    return (temperature >= TEMP_MIN && temperature <= TEMP_MAX &&
-            soc >= SOC_MIN && soc <= SOC_MAX &&
-            chargeRate <= CHARGE_RATE_MAX);
+    return checkTemperature(temperature) &&
+           checkStateOfCharge(soc) &&
+           checkChargeRate(chargeRate);
 }
 
 int main() {
