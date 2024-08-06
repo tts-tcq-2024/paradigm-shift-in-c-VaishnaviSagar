04e3dbbaffae7926118e6bfe0d2b9dc596f62412
VaishnaviSagar <67277305+VaishnaviSagar@users.noreply.github.com>
2024-08-06 10:44:38 +0530

    Create BatteryStatus.c

---
+++ BatteryStatus.c
@@ -0,0 +1,16 @@
+#include <BatteryStatus.h>
+BatteryStatus mapToBatteryStatus(float value, float lower, float upper) {
+    if (value < lower) {
+        return LOW_SOC_BREACH;
+    }
+    if (value >= lower && value <= lower + 3) {
+        return LOW_SOC_WARNING;
+    }
+    if (value > lower + 3 && value < upper - 5) {
+        return NORMAL;
+    }
+    if (value >= upper - 5 && value <= upper) {
+        return HIGH_SOC_WARNING;
+    }
+    return HIGH_SOC_BREACH;
+}
